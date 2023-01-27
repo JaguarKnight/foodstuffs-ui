d2fd38ba50ba7c08bf1ee88c4288e1736a4b9046
JaguarKnight <jonathangreer44@gmail.com>
2023-01-26 18:18:39 -0600

    added tailwindcss; cleared out app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Foodstuff, FoodstuffsService } from './services/foodstuffs.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   title = 'foodstuffs-ui';
